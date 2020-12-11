9142263ed631466fc3ce2f84c57ca6bd8f459557
gevari <alex.madeforwork@gmail.com>
2020-12-11 11:41:40 +0100

    Removed Bug

--- src/Simplic.Configuration.Data.DB/Properties/AssemblyInfo.cs
+++ src/Simplic.Configuration.Data.DB/Properties/AssemblyInfo.cs
@@ -1,4 +1,4 @@
-﻿using System.Reflection;
+using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("7.0.120.1006")]
-[assembly: AssemblyFileVersion("7.0.120.1006")]
+[assembly: AssemblyVersion("7.0.220.1006")]
+[assembly: AssemblyFileVersion("7.0.220.1006")]
